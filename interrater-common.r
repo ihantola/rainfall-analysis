0fcedd2c8dfb9b70062c170f85d7969b1b5fa0be
Petri Ihantola <petri.ihantola@tut.fi>
2015-04-21 09:48:27 -0700

    Commenting out unused parts and adding iota for reduced True/False data (plans are either ok or not).

--- interrater-common.r
+++ interrater-common.r
@@ -1,51 +1,63 @@
 ## First, looking all categories independently
 
-print("**** Light interrater reliabilities ******")
-print("Sentinel **********************************")
-kappam.light(Sentinel)
-print("Negs **************************************")
-kappam.light(Negs)
-print("Sum ***************************************")
-print("irr library fails to calculate this, perferct agreement")
-#kappam.light(Sum)
-print("Count *************************************")
-kappam.light(Count)
-print("Divzero ***********************************")
-kappam.light(Divzero)
-print("Avg ***************************************")
-print("irr library fails to calculate this, perferct agreement")
-#kappam.light(Avg)
+## print("**** Light interrater reliabilities ******")
+## print("Sentinel **********************************")
+## kappam.light(Sentinel)
+## print("Negs **************************************")
+## kappam.light(Negs)
+## print("Sum ***************************************")
+## print("irr library fails to calculate this, perferct agreement")
+## #kappam.light(Sum)
+## print("Count *************************************")
+## kappam.light(Count)
+## print("Divzero ***********************************")
+## kappam.light(Divzero)
+## print("Avg ***************************************")
+## print("irr library fails to calculate this, perferct agreement")
+## #kappam.light(Avg)
 
 ## Then, combine plans together - each combination is one category
-combined <- cbind(Sentinel,Negs,Sum,Count,Divzero,Avg)
+## combined <- cbind(Sentinel,Negs,Sum,Count,Divzero,Avg)
 
-combined$rater1 <- do.call(paste, as.list(combined[,c(1,4,7,10,13,16)])) 
-combined$rater2 <- do.call(paste, as.list(combined[,c(2,5,8,11,14,17)])) 
-combined$rater3 <- do.call(paste, as.list(combined[,c(3,6,9,12,15,18)]))
+## combined$rater1 <- do.call(paste, as.list(combined[,c(1,4,7,10,13,16)])) 
+## combined$rater2 <- do.call(paste, as.list(combined[,c(2,5,8,11,14,17)])) 
+## combined$rater3 <- do.call(paste, as.list(combined[,c(3,6,9,12,15,18)]))
 
-print("")
-print("***************************************")
-print("combination of the plans as one observation - light")
+## print("")
+## print("***************************************")
+## print("combination of the plans as one observation - light")
 
-print(kappam.light(combined[,c('rater1','rater2','rater3')]))
+## print(kappam.light(combined[,c('rater1','rater2','rater3')]))
+
 print("combination of the plans as one observation - iota")
 print(iota(list(Sentinel, Negs, Sum, Count, Divzero, Avg)))
+
+print("Convert all the data to TRUE/FALSE - either plans are ok or they are not and do the same analysis for these reduced levels")
+Sentinel = Sentinel=="ok"
+Negs = Negs=="ok"
+Sum = Sum=="ok"
+Count = Count=="ok"
+Divzero = Divzero=="ok"
+Avg = Avg=="ok"
+
+print("combination of the plans as one observation - iota")
+print(iota(list(Sentinel, Negs, Sum, Count, Divzero, Avg), scaledata="n"))
 
 
 ### icc for the grade, arguments
 ## 't' two way as graders are not random
 ## 'a' agreement means that we would like to seek whether grades are the same, i.e. even consistent bias is not allowed
 ## 's' Estimating the reliability of single measure - vs. 'a' that would estimate the reliability of the average of the graders
-print("")
-print("***************************************")
-print("ICC for the grade")
-print(icc(Grade,'t','a','s'))
+## print("")
+##print("***************************************")
+##print("ICC for the grade")
+##print(icc(Grade,'t','a','s'))
 
-Grade$A <- Grade$A>17
-Grade$E <- Grade$E>17
-Grade$J <- Grade$J>17
-print("")
-print("***************************************")
-print("All plans ok or not?")
+##Grade$A <- Grade$A>17
+##Grade$E <- Grade$E>17
+##Grade$J <- Grade$J>17
+##print("")
+##print("***************************************")
+##print("All plans ok or not?")
 #kappam.light(Grade)
-print(icc(Grade,'t','a','s'))
+##print(icc(Grade,'t','a','s'))
